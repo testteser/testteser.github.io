ba9c8e4ffc7c4b4e01672607a9dc0b94c6484b16
testteser <98735222+testteser@users.noreply.github.com>
2022-09-05 19:35:03 +0500

    Add files via upload

---
+++ steps.js
@@ -0,0 +1,52 @@
+$(function () {
+    const submitButton = $('#main-form-button'),
+          mainForm = $('#main-form'),
+          mainFormInputs = $('.data-form__item-input')
+
+    mainFormInputs.each(function (index, item) {
+        $(item).on('input', function () {
+            $(item).closest('.data-form__item').removeClass('error-field')
+
+            if ($(this).val().length > 0) {
+                $(this).closest('.data-form__item').addClass('success-field')
+            } else {
+                $(this).closest('.data-form__item').removeClass('success-field')
+            }
+        })
+
+        $(item).on('blur', function () {
+            if ($(this).val().length <= 0) {
+                $(this).closest('.data-form__item').removeClass('success-field')
+                $(this).closest('.data-form__item').addClass('error-field')
+            }
+        })
+    })
+
+    mainForm.on('submit', function (e) {
+        e.preventDefault()
+
+        if (!getInvalidInputs().length) {
+            submitButton.toggleClass('active')
+        } else {
+            getInvalidInputs().each(function (index, item) {
+                $(item).closest('.data-form__item').addClass('error-field')
+            })
+        }
+    })
+
+    function getInvalidInputs() {
+        return mainFormInputs.map(function (index, item) {
+            if ($(item).val().length <= 0) {
+                return $(item)
+            }
+        })
+    }
+
+    function getValidInputs() {
+        return mainFormInputs.map(function (index, item) {
+            if ($(item).val().length > 0) {
+                return $(item)
+            }
+        })
+    }
+})
