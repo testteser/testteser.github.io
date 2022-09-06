554379846e926c983e5586c28e2b4a5b8d1d122b
hiztn <studyfullstack777@gmail.com>
2022-09-06 23:11:01 +0500

    12

--- steps.js
+++ steps.js
@@ -93,15 +93,4 @@ $(function () {
             $(item).closest('.data-form__item').addClass('success-field')
         })
     })
-
-    function isiOS() { return navigator.userAgent.match(/ipad|ipod|iphone/i); }
-
-    if (isiOS()){
-        var ins = [], _ins = document.querySelectorAll("input")
-        for(var i = 0; i <ins.length; i++) ins.push(_ins[i])
-        document.body.addEventListener('touchstart', event => {
-            if(ins.filter(i => i.contains(event.target)).length == 0)
-                document.activeElement.blur()
-        });
-    }
 })
